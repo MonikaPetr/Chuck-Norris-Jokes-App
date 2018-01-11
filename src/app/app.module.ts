6454bdf339be67dfe0cb512ad146339c2b62ccb3
MonikaPetr <petroniene.m@gmail.com>
2018-01-11 16:59:02 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { ChuckComponent } from './chuck/chuck.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatOptionModule, MatSelectModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+
+
+
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChuckComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatOptionModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/chuck/chuck.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-chuck',
+  templateUrl: './chuck.component.html',
+  styleUrls: ['./chuck.component.scss']
+})
+export class ChuckComponent implements OnInit {
+  categories:any;
+  joke:any;
+  jokes = [];
+  minInput: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('https://api.chucknorris.io/jokes/categories').subscribe(categories => {
+      this.categories = categories;
+    });
+    this.http.get('https://api.chucknorris.io/jokes/random').subscribe(random => {
+      this.jokes.push(random);
+    })
+  }
+
+  onChange(category) {
+    this.http.get('https://api.chucknorris.io/jokes/random?category='+category).subscribe(joke => {
+      this.jokes = [];
+      this.jokes.push(joke);
+      console.log(joke);
+    });
+  }
+
+  onSearchChange(searchValue){
+if (searchValue.length > 3) {
+  this.http.get('https://api.chucknorris.io/jokes/search?query='+searchValue).subscribe(result => {
+    this.jokes = result.result;
+    console.log(result);
+  });
+}
+
+
+
+  }
+
+}
